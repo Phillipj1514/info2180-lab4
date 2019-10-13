ec9e7f67d8b81c603d1f1797358cc589379e1eda
phillipj1514 <phillipjunior95@gmail.com>
2019-10-13 02:18:58 -0500

    add click listener to each block

--- js/tic-tac-toe.js
+++ js/tic-tac-toe.js
@@ -1,10 +1,27 @@
+let blocks = [];
+let userType = -1;
 window.addEventListener('DOMContentLoaded', fill_Squares);
 
 
 function fill_Squares(){
-	let blocks = document.getElementById("board").querySelectorAll("div")
-	for(let a = 0; a <blocks.length; a++){
-		blocks[a].classList.add("square")
+	let board_div = document.getElementById("board").querySelectorAll("div")
+	for(let a = 0; a <board_div.length; a++){
+		blocks[a] = {name: board_div[a], value:""}
+		blocks[a].name.classList.add("square")
+		// add click listener to each block
+		blocks[a].name.addEventListener("click", change_block)
+	}	
+}
+
+function change_block(){
+	if(userType === -1){
+		userType  = Math.floor(Math.random() * (10 - 1) ) + 1;
 	}
-	console.log(blocks);
+	if(userType <= 5){
+		this.classList.add("X");
+		this.innerHTML = "X";
+	}else{
+		this.classList.add("O");
+		this.innerHTML = "O";
+	}
 }
