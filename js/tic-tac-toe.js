ad1fb6e2c0b8b3dfa28e8d0392e42796b465939a
phillipj1514 <phillipjunior95@gmail.com>
2019-10-13 02:56:09 -0500

    Added the hover effect

--- js/tic-tac-toe.js
+++ js/tic-tac-toe.js
@@ -4,24 +4,53 @@ window.addEventListener('DOMContentLoaded', fill_Squares);
 
 
 function fill_Squares(){
-	let board_div = document.getElementById("board").querySelectorAll("div")
-	for(let a = 0; a <board_div.length; a++){
-		blocks[a] = {name: board_div[a], value:""}
-		blocks[a].name.classList.add("square")
-		// add click listener to each block
-		blocks[a].name.addEventListener("click", change_block)
-	}	
-}
-
-function change_block(){
 	if(userType === -1){
 		userType  = Math.floor(Math.random() * (10 - 1) ) + 1;
 	}
+	let board_div = document.getElementById("board").querySelectorAll("div")
+	for(let a = 0; a <board_div.length; a++){
+		blocks[a] = {name: board_div[a], value:""};
+		//add an id to each div
+		blocks[a].name.id = a;
+		//add the square property to each div
+		blocks[a].name.classList.add("square");
+		// add click listener to each block
+		blocks[a].name.addEventListener("click", set_block);
+		// add on mouseover listener and mouseout listener for hover effect
+		blocks[a].name.addEventListener("mouseover", hover_set_block);
+		blocks[a].name.addEventListener("mouseout", unset_block);
+	}	
+}
+
+function set_block(){
+	this.classList.remove("hover");
 	if(userType <= 5){
 		this.classList.add("X");
 		this.innerHTML = "X";
+		blocks[this.id].value = "X" 
 	}else{
 		this.classList.add("O");
 		this.innerHTML = "O";
+		blocks[this.id].value = "O" 
+
+	}
+	console.log(this.id)
+	console.log(blocks[this.id])
+}
+function hover_set_block(){
+	if(blocks[this.id].value === ""){
+		if(userType <= 5){
+			this.classList.add("X","hover");
+			this.innerHTML = "X";
+		}else{
+			this.classList.add("O","hover");
+			this.innerHTML = "O";
+		}
+	}
+}
+function unset_block(){
+	if(blocks[this.id].value === ""){
+		this.classList.remove("X","O","hover");
+		this.innerHTML = "";
 	}
 }
